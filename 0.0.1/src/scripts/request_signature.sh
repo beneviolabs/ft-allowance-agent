f5c65d93a7847ccd950183850f61b93388c0620d
charles <charleslavong@gmail.com>
2025-03-19 17:12:01 -0400

    include sufficient gas for each transaction and calcuate the nonce by incrementing the latest nonce value of the key used for signing

--- 0.0.1/src/scripts/request_signature.sh
+++ 0.0.1/src/scripts/request_signature.sh
@@ -15,15 +15,35 @@ echo "Using agent ID: $AGENT_ID"
 # Store block hash from parameter
 BLOCK_HASH="$1"
 
-# Calculate nonce from current timestamp in milliseconds
-NONCE=$(date +%s | cut -b1-13)
+# Fetch the current nonce from the mpc public key
+NONCE=$(curl -s -X POST https://rpc.testnet.near.org \
+    -H 'Content-Type: application/json' \
+    -d '{
+        "jsonrpc": "2.0",
+        "id": "dontcare",
+        "method": "query",
+        "params": {
+            "request_type": "view_access_key",
+            "finality": "final",
+            "account_id": "'"$AGENT_PROXY_ACCOUNT"'",
+            "public_key": "'"$MPC_DERIVED_PK"'"
+        }
+    }' | grep -o '"nonce":[0-9]*' | grep -o '[0-9]*')
 
+if [ -z "$NONCE" ]; then
+    echo "Failed to extract current nonce from RPC response"
+    exit -1
+fi
+
+# Increase nonce by 10
+NONCE=$((NONCE + 10))
+echo "Using nonce: $NONCE"
 # Transaction parameters
 ARGS='{
     "contract_id": "wrap.testnet",
     "method_name": "near_deposit",
     "args": [],
-    "gas": "0",
+    "gas": "300000000000000",
     "deposit": "1000000000000000000000000",
     "nonce": "'"$NONCE"'",
     "block_hash": "'"$BLOCK_HASH"'",
@@ -33,4 +53,5 @@ ARGS='{
 # Make the contract call
 near call $AGENT_PROXY_ACCOUNT request_signature "$ARGS" \
     --accountId $AGENT_ID \
-    --deposit 1
+    --deposit 1 \
+    --gas 50000000000000
