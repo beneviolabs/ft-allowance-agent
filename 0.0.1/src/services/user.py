698e14fbb6ae083884c1f49cfe3c90686fd4bc94
Ryan Marvin <marvinryan@ymail.com>
2025-04-22 23:51:16 +0100

    Refactor get_or_create

--- 0.0.1/src/services/user.py
+++ 0.0.1/src/services/user.py
@@ -1,19 +1,28 @@
 from src.models.user import User
 from sqlalchemy.orm import Session
+from sqlalchemy import exc
 
 from src.common import DivvyGoalType
 from decimal import Decimal
 
 
-def create_user(db: Session, user: User) -> User:
+def get_or_create_user(db: Session, near_id: str) -> User | None:
     """
-    Create a new user in the database.
+    Create a new user in the database. Returns the user if it they already exist.
     """
     user = User(
-        near_account_id=user.near_account_id,
+        near_account_id=near_id,
     )
     db.add(user)
-    db.commit()
+    try:
+        db.commit()
+    except exc.IntegrityError as err:
+        if 'unique constraint "ix_users_near_account_id"' in str(err):
+            db.rollback()
+            return get_user_by_near(db, near_id)
+        else:
+            raise
+
     return user
 
 
