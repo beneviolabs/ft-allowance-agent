392546ac5d9d0d8e56a4ba42f56b5d46a1677bda
charles <charleslavong@gmail.com>
2025-06-09 16:03:50 -0400

    reducing the delta from main

--- contracts/utils.rs
+++ contracts/utils.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use near_sdk::PublicKey;
 use omni_transaction::near::types::PublicKey as OmniPublicKey;
 use omni_transaction::near::types::{ED25519PublicKey, Secp256K1PublicKey};
@@ -10,29 +12,27 @@ pub fn hash_payload(payload: &[u8]) -> [u8; 32] {
 }
 
 pub fn convert_pk_to_omni(pk: &String) -> omni_transaction::near::types::PublicKey {
-    let (curve_type, trimmed_pk) = if pk.starts_with("ed25519:") {
+    let (curve_type, pub_key) = if pk.starts_with("ed25519:") {
         (
             near_sdk::CurveType::ED25519,
-            pk.strip_prefix("ed25519:").unwrap(),
+            &PublicKey::from_str(pk).unwrap(),
         )
     } else if pk.starts_with("secp256k1:") {
         (
             near_sdk::CurveType::SECP256K1,
-            pk.strip_prefix("secp256k1:").unwrap(),
+            &PublicKey::from_str(pk).unwrap(),
         )
     } else {
         panic!("Invalid public key format: {}", pk)
     };
 
-    let pk_parts = bs58::decode(trimmed_pk).into_vec().unwrap();
-    let pub_key: near_sdk::PublicKey = PublicKey::from_parts(curve_type, pk_parts).unwrap();
-    let public_key_data = &pub_key.as_bytes()[1..]; // Skip curve type byte
+    let public_key_data = &pub_key.as_bytes()[1..]; // Skipping the first byte which is the curve type
 
     match curve_type {
         near_sdk::CurveType::ED25519 => {
             const ED25519_PUBLIC_KEY_LENGTH: usize = 32;
             let ed25519_key: [u8; ED25519_PUBLIC_KEY_LENGTH] =
-                public_key_data.try_into().unwrap_or_else(|_| {
+                (*public_key_data).try_into().unwrap_or_else(|_| {
                     panic!(
                         "Failed to convert ED25519 key, expected length {}, got {}",
                         ED25519_PUBLIC_KEY_LENGTH,
@@ -44,7 +44,7 @@ pub fn convert_pk_to_omni(pk: &String) -> omni_transaction::near::types::PublicK
         near_sdk::CurveType::SECP256K1 => {
             const SECP256K1_PUBLIC_KEY_LENGTH: usize = 64;
             let secp_key: [u8; SECP256K1_PUBLIC_KEY_LENGTH] =
-                public_key_data.try_into().unwrap_or_else(|_| {
+                (*public_key_data).try_into().unwrap_or_else(|_| {
                     panic!(
                         "Failed to convert SECP256K1 key, expected length {}, got {}",
                         SECP256K1_PUBLIC_KEY_LENGTH,
