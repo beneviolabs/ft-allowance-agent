c710820f43ba9de7edf281e1abed2106be1d4244
charles <charleslavong@gmail.com>
2025-09-15 15:36:35 -0400

    remove the ability to deploy a locally stored wasm

--- contracts/factory/factory.rs
+++ contracts/factory/factory.rs
@@ -4,8 +4,6 @@ use near_sdk::{
     AccountId, Gas, NearToken, PanicOnDefault, Promise, PromiseError, PublicKey, env, near,
 };
 
-const NEAR_PER_STORAGE: NearToken = NearToken::from_yoctonear(10u128.pow(19));
-const PROXY_CODE: &[u8] = include_bytes!("../target/near/proxy_contract.wasm");
 const TESTNET_SIGNER: &str = "v1.signer-prod.testnet";
 const MAINNET_SIGNER: &str = "v1.signer";
 
@@ -73,62 +71,6 @@ impl ProxyFactory {
             .create_account()
             .transfer(env::attached_deposit())
             .use_global_contract(self.global_proxy_base58_hash.clone())
-            .function_call(
-                "new".to_string(),
-                near_sdk::serde_json::to_vec(&ProxyInitArgs {
-                    owner_id,
-                    signer_id: self.signer_contract.clone(),
-                })
-                .unwrap(),
-                NearToken::from_near(0),
-                Gas::from_tgas(50),
-            )
-    }
-
-    #[payable]
-    pub fn deposit_and_create_proxy(&mut self, owner_id: AccountId) -> Promise {
-        let deposit = env::attached_deposit();
-        assert!(
-            //TODO update this to use NEP-591 to reduce the required near. https://github.com/near/NEPs/pull/591/files
-            deposit >= NearToken::from_yoctonear(3_800_000_000_000_000_000_000_000),
-            "Must attach at least 3.8 NEAR"
-        );
-
-        self.create_proxy(owner_id.clone()).then(
-            Self::ext(env::current_account_id())
-                .on_proxy_created(env::predecessor_account_id(), deposit),
-        )
-    }
-
-    #[payable]
-    pub fn create_proxy(&mut self, owner_id: AccountId) -> Promise {
-        assert!(
-            env::attached_deposit() >= NEAR_PER_STORAGE,
-            "Attach at least 1 yⓃ"
-        );
-        let trimmed_owner = self.get_base_account_name(&owner_id);
-        let full_sub_account: AccountId =
-            format!("{}.{}", trimmed_owner, env::current_account_id())
-                .parse()
-                .unwrap();
-
-        // Verify deployment conditions
-        let attached = env::attached_deposit();
-        let code = PROXY_CODE;
-        let contract_bytes = code.len() as u128;
-        let contract_storage_cost = NEAR_PER_STORAGE.saturating_mul(contract_bytes);
-        let minimum_needed = contract_storage_cost.saturating_add(NearToken::from_millinear(100));
-
-        assert!(
-            attached >= minimum_needed,
-            "Attach at least {minimum_needed} yⓃ"
-        );
-
-        // This is one transaction with multiple action receipts. The entire transaction will be "rolled-back" if an action fails, like https://testnet.nearblocks.io/txns/2oQzUR7RV4v69t7VZaLP8AiZBrh3rdULVMQx8bury9A6
-        Promise::new(full_sub_account.clone())
-            .create_account()
-            .transfer(env::attached_deposit())
-            .deploy_contract(code.to_vec())
             .function_call(
                 "new".to_string(),
                 near_sdk::serde_json::to_vec(&ProxyInitArgs {
