6c4f753d6cb037038579e52d676cbba8a88cc32f
charles <charleslavong@gmail.com>
2025-09-15 16:20:52 -0400

    increase the minimum balance to pay the required storage of the factory account itself, not the proxy

--- contracts/factory/factory-deploy.sh
+++ contracts/factory/factory-deploy.sh
@@ -52,7 +52,7 @@ echo "Building contract..."
 RUSTFLAGS="-Z unstable-options" cargo +nightly near build non-reproducible-wasm --no-abi
 
 # Set variables
-# https://testnet.nearblocks.io/txns/698pLN7YVxSAMazuTWsFDsNy6u94xp2Zv8HUWjcobgeT?tab=execution
+# https://nearblocks.io/txns/6iyX1GMA3wGrh2qtz8hKv7ppf1ngV83nKiz8ZKDAE71a
 #near contract deploy-as-global use-file /Users/charles/.nearai/registry/charleslavon.near/ft-allowance/contracts/target/near/proxy_contract.wasm as-global-hash peerfolio.testnet network-config testnet sign-with-keychain send
 GLOBAL_PROXY_CODE_HASH="CxhHoMAytiy39MSyKCJRksiWXvhYdRYncFpcVWAd4Pbg"
 WASM_PATH="target/near/proxy_factory.wasm"
@@ -83,7 +83,7 @@ fi
 # Deploy factory
     echo "Deploying factory contract..."
 if ! near state "$FACTORY_ACCOUNT" &>/dev/null; then
-    near create-account "$FACTORY_ACCOUNT" --masterAccount "$FACTORY_OWNER" --initialBalance 0.25
+    near create-account "$FACTORY_ACCOUNT" --masterAccount "$FACTORY_OWNER" --initialBalance 1.5
 
     echo "Waiting 2 seconds for block finality before deploying..."
     sleep 2
